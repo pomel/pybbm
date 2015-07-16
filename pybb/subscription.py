113f998b4ecf89b5b30a918cbfc5b2417193fdb9
Voronkov <mail@voronkovd.ru>
2015-07-16 14:27:34 +0600

    add TopicEmailSendUserNotification in view

--- pybb/subscription.py
+++ pybb/subscription.py
@@ -23,41 +23,38 @@ def notify_topic_subscribers(post):
     topic = post.topic
     if post != topic.head:
         old_lang = translation.get_language()
-
-        # Define constants for templates rendering
         delete_url = reverse('pybb:delete_subscription', args=[post.topic.id])
         current_site = Site.objects.get_current()
         from_email = settings.DEFAULT_FROM_EMAIL
 
-        subject = render_to_string('pybb/mail_templates/subscription_email_subject.html',
-                                   {'site': current_site,
-                                    'post': post})
-        # Email subject *must not* contain newlines
+        subject = render_to_string(
+            'pybb/mail_templates/subscription_email_subject.html',
+            {'site': current_site,
+             'post': post})
         subject = ''.join(subject.splitlines())
-
         mails = tuple()
         for user in topic.subscribers.exclude(pk=post.user.pk):
-            try:
-                validate_email(user.email)
-            except:
-                # Invalid email
-                continue
+            notifications = post.topic.notifications.filter(topic=topic,
+                                                            user=user)
+            if not notifications:
+                post.topic.notifications.create(topic=topic, user=user)
+                try:
+                    validate_email(user.email)
+                except:
+                    continue
+                if (user.email == '%s@example.com' % getattr(
+                        user, compat.get_username_field())):
+                    continue
 
-            if user.email == '%s@example.com' % getattr(user, compat.get_username_field()):
-                continue
-
-            lang = util.get_pybb_profile(user).language or settings.LANGUAGE_CODE
-            translation.activate(lang)
-
-            message = render_to_string('pybb/mail_templates/subscription_email_body.html',
-                                       {'site': current_site,
-                                        'post': post,
-                                        'delete_url': delete_url,
-                                        'user': user})
-            mails += ((subject, message, from_email, [user.email]),)
-
-        # Send mails
+                lang = (util.get_pybb_profile(user).language or
+                        settings.LANGUAGE_CODE)
+                translation.activate(lang)
+                message = render_to_string(
+                    'pybb/mail_templates/subscription_email_body.html',
+                    {'site': current_site,
+                     'post': post,
+                     'delete_url': delete_url,
+                     'user': user})
+                mails += ((subject, message, from_email, [user.email]),)
         send_mass_mail(mails, fail_silently=True)
-
-        # Reactivate previous language
         translation.activate(old_lang)
