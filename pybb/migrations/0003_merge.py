42ced6d3a81f7d932d059dc933e0c80213f4bd7b
ttyS15 <alex.manaev@gmail.com>
2015-07-18 00:47:46 +0300

    #666 Translation

---
+++ pybb/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pybb', '0002_auto_20150705_1602'),
+        ('pybb', '0002_auto_20150715_0227'),
+    ]
+
+    operations = [
+    ]
